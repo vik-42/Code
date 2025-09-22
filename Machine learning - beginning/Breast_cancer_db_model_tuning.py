bfe8523bec53d8cb2ac595746d0794c05b8bb877
vik-42s <vittoriopt@yahoo.it>
2025-09-22 16:00:36 -0500

    Added GridSearchCV for fine tuning of hyper parameters

--- Machine learning - beginning/Breast_cancer_db_model_tuning.py
+++ Machine learning - beginning/Breast_cancer_db_model_tuning.py
@@ -84,3 +84,21 @@ plt.ylim([0.8,1.0])
 plt.legend(loc ='lower right')
 plt.show()
 
+#There is a slight overfit when incrementing C over 0.1, which is the optimal value. To better optimize the C parameter Grid-Search is implemented (computationally expensive);
+#A less expensive approach could be achieved by using RandomizedSearchCV;
+#A Support Vector Machine model is used this time:
+
+pipe_svc = Pipeline([('scl', StandardScaler()), ('clf', SVC(random_state=1))])
+param_range = [0.0001, 0.001, 0.01, 0.1, 1.0, 10.0, 100.0, 1000.0]
+param_grid = [ {'clf__C' : param_range , 'clf__kernel' : ['linear']},
+              {'clf__C' : param_range , 'clf__gamma':param_range, 'clf__kernel':['rbf']}]
+gs = GridSearchCV(estimator = pipe_svc, param_grid=param_grid, scoring='accuracy', cv=10,n_jobs=-1)
+gs = gs.fit(X_train, y_train)
+
+print('The best score is %3f \n' %gs.best_score_)
+print('The best parameters are %s'% gs.best_params_)
+
+
+clf = gs. best_estimator_
+clf.fit(X_train, y_train)
+print('Test accurac of the best model: %.3f'%clf.score(X_train, y_train))
